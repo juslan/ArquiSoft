305b4b2441044db9744d4abcf19044d924a8a4ea
mabatule1999 <maba1998a@gmail.com>
2021-03-27 12:08:17 -0400

    Codigo refactorizado

--- DirtyCodeCalculadora/main.cpp
+++ DirtyCodeCalculadora/main.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
-//ffffffffff
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
 using namespace std;
-
+float calculadora(int num1, int num2,char caracter){
+     int res;
+     if(caracter=='+'){
+        res=num1+num2;
+     }
+     if(caracter=='*'){
+        res=num1*num2;
+     }
+     if(caracter=='/'){
+       res=num1/num2;
+     }
+     if(caracter=='-'){
+        res=num1-num2;
+     }
+   return res;
+}
 int main()
 {
-    while(true){
-            int a,b,c;
-        char d;
-        cin>>a>>b>>d;
-        if(d=='+'){
-            c=a+b;
-        }
-        else{
-            if(d=='-'){
-                c=a-b;
-            }
-            else{
-                if(d=='*'){
-                    c=a*b;
-                }
-                else{
-                    if(d=='/'){
-                        c=a/b;
-                    }
-                }
-            }
-        }
-        cout << c << endl;
+    int num1,num2;
+    char caracter;
+    while(caracter!='x'){
+
+         cout<<"Ingrese en el siguiente formato"<<endl;
+         cout<<"Primer numero/ Segundo numero / Operacion(+,-,*,/)"<<endl;
+         cout<<"Ej: 1 * 2  >> devolvera 2"<<endl;
+         cin>>num1>>caracter>>num2;
+         if(caracter=='+' || caracter=='-' ||caracter=='/' ||caracter=='*'){
+                cout << calculadora(num1,num2,caracter) << endl;
+         }else{
+                cout<<"ERROR CARACTER NO ADMITIDO"<<endl;
+         }
+        cout<<"Programa acaba si pone 'x' caso contrario presion cualquier caracter"<<endl;;
+        caracter=getch();
+        system("cls");
     }
     return 0;
 }
